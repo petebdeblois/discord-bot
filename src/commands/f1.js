4cd17f78679d9c66a202c0e0d57979441330c60f
petebdeblois <petetebdeblois@gmail.com>
2020-08-22 21:17:33 -0400

    doesnt work yet

--- src/commands/f1.js
+++ src/commands/f1.js
@@ -1,43 +1,74 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 
-
+var axios = require('axios');
+const {
+    json
+} = require('express');
 // const trim = (str, max) => str.length > max ? `${str.slice(0, max - 3)}...` : str;
 
 
 module.exports = async (msg) => {
+    var config = {
+        method: 'get',
+        url: 'http://ergast.com/api/f1/current/last/results.json',
+        headers: {},
+    };
 
-
-    const {
-        list
-    } = await fetch(
-        'http://ergast.com/api/f1/current/last/results.json'
-    ).then(response => response.json())
-        .then(data => console.log(data))
-        .then(data => print(data));
-    //msg.channel.send(list);
-    //console.log('list' + JSON.parse(list));
-    if (!list.length) {
-        return msg.channel.send('List is undefined ');
-    }
-    //const [answer] = list;
-    function print(list) {
-        const embed = new Discord.MessageEmbed()
-            .setColor('#e134eb')
-            .setTitle(list.MRData.series)
-            .setURL(list.url);
-        /*
-        .addFields({
-            name: 'Definition',
-            value: trim(answer.definition, 1024),
-        }, {
-            name: 'Example',
-            value: trim(answer.example, 1024),
-        }, {
-            name: 'Rating',
-            value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`,
+    const data = axios(config)
+        .then(function (response) {
+            let datap = JSON.stringify(response.data);
+            console.log(datap);
+            const embed = new Discord.MessageEmbed()
+                .setColor('#e134eb')
+                .setTitle(datap)
+                .setURL(data.url);
+            /*
+    .addFields({
+        name: 'Definition',
+        value: trim(answer.definition, 1024),
+    }, {
+        name: 'Example',
+        value: trim(answer.example, 1024),
+    }, {
+        name: 'Rating',
+        value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`,
+    });
+    */
+            msg.channel.send(embed);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
-        */
-        msg.channel.send(embed);
-    }
+
+    /*
+    const MRData = await fetch(
+            'http://ergast.com/api/f1/current/last/results.json'
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    */  
+    //const [answer] = data;
+    /*
+    const embed = new Discord.MessageEmbed()
+        .setColor('#e134eb')
+        .setTitle(data.MRData.series)
+        .setURL(data.url);
+
+    .addFields({
+        name: 'Definition',
+        value: trim(answer.definition, 1024),
+    }, {
+        name: 'Example',
+        value: trim(answer.example, 1024),
+    }, {
+        name: 'Rating',
+        value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`,
+    });
+   
+    msg.channel.send(embed);
+ */
 };
